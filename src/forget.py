500392f8cdde0710790de8a0e506235bb59face6
umityigitbsrn <umityigitbsrn@gmail.com>
2024-12-14 20:11:12 -0800

    forget for linear classifiers are implementeed

--- src/forget.py
+++ src/forget.py
@@ -43,4 +43,95 @@
 #     for (data, label) in loader:
 #         curr_grad, curr_hess = compute_batch_grad_hess(model, criterion, data, label, flat_params)
 
+import torch
+from copy import deepcopy
 
+from src.utils import get_module_device
+
+
+def calculate_cov(loader, device):
+    cumulative_cov = None
+    cumulative_size = 0
+    for data, _ in loader:
+        data = data.to(device)
+        if cumulative_cov is None:
+            cumulative_cov = (data.T @ data).clone().detach().to('cpu')
+        else:
+            cumulative_cov += (data.T @ data).clone().detach().to('cpu')
+        cumulative_size = cumulative_size + data.shape[0]
+    return cumulative_cov / cumulative_size
+
+
+def calculate_hess(model, loader, criterion, linear=False, num_class=None):
+    device = get_module_device(model)
+    if linear and num_class is not None:
+        hess = torch.kron(torch.eye(num_class), calculate_cov(loader, device))
+        return hess
+
+
+def _accumulate_grads(prev, curr, prev_size, curr_size):
+    if prev is None:
+        return curr
+    else:
+        res = []
+        for idx in range(len(prev)):
+            res.append((prev[idx] * prev_size + curr[idx] * curr_size) / (prev_size + curr_size))
+        return res
+
+
+def calculate_grad(model, loader, criterion):
+    device = get_module_device(model)
+    grads = None
+    total_size = 0
+    for data, label in loader:
+        data, label = data.to(device), label.to(device)
+        model.zero_grad()
+        output = model(data)
+        loss = criterion(output, label)
+        loss.backward()
+        curr_grads = [param.grad.clone().detach().to('cpu') for param in model.parameters()]
+        grads = _accumulate_grads(grads, curr_grads, total_size, total_size + data.shape[0])
+        total_size += data.shape[0]
+    return grads
+
+
+def _linearize_grads(grads):
+    flat_grad = [grad.view(-1) for grad in grads]
+    prev_sizes = [grad.size() for grad in grads]
+    return torch.cat(flat_grad), prev_sizes
+
+
+def _adjust_update(update, prev_sizes):
+    splits = [size[0] * size[1] for size in prev_sizes]
+    adjusted_update = torch.split(update, splits)
+    adjusted_update = [adjusted_update[idx].view(prev_sizes[idx]) for idx in range(len(prev_sizes))]
+    return adjusted_update
+
+
+def calculate_update(hessian, grads, device):
+    flat_grad, prev_sizes = _linearize_grads(grads)
+    flat_grad = flat_grad.to(device)
+    eps = 1e-4
+    hess = (hessian + eps * torch.eye(hessian.shape[0])).to(device)
+    inv = torch.linalg.inv(hess)
+    update = torch.mv(inv, flat_grad).to('cpu')
+    update = _adjust_update(update, prev_sizes)
+    return update
+
+
+def update_model(model, updates):
+    params = [param for param in model.parameters()]
+    device = get_module_device(model)
+    with torch.no_grad():
+        for param, update in zip(params, updates):
+            param.add_(update.to(device))
+
+
+def forget(model, hess_loader, grad_loader, criterion, linear=False, num_class=None):
+    device = get_module_device(model)
+    fmodel = deepcopy(model.to('cpu')).to(device)
+    hess = calculate_hess(fmodel, hess_loader, criterion, linear=linear, num_class=num_class)
+    grads = calculate_grad(fmodel, grad_loader, criterion)
+    update = calculate_update(hess, grads, device)
+    update_model(fmodel, update)
+    return fmodel
--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,10 @@ def set_seed(seed: int = 42) -> None:
     torch.manual_seed(seed)
     random.seed(seed)
     np.random.seed(seed)
+
+
+def get_module_device(module):
+    try:
+        return next(module.parameters()).device
+    except StopIteration:
+        return next(module.buffers()).device
