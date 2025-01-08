0df67c46019660ce48d5adae19b61704bd40a213
umityigitbsrn <umityigitbsrn@gmail.com>
2025-01-07 16:43:21 -0800

    unknown main is implemented if it seems ok the experiments will be conducted

--- src/data.py
+++ src/data.py
@@ -1,8 +1,15 @@
-from torchvision.datasets import MNIST, USPS, CIFAR10
-from torch.utils.data import DataLoader, Dataset, random_split, Subset, ConcatDataset
+from torchvision.datasets import MNIST, USPS, CIFAR10, ImageFolder
+from torch.utils.data import DataLoader, Dataset, random_split, Subset, ConcatDataset, TensorDataset
 import numpy as np
+import torch
+from torchvision.models import ResNet18_Weights, resnet18
+from torchvision.transforms import v2
+from torchvision.transforms.v2.functional import InterpolationMode
 
 from typing import Tuple
+import logging
+import os
+from tqdm import tqdm
 
 
 def get_dataloaders(datasets,
@@ -18,7 +25,8 @@ def get_dataloaders(datasets,
         return dataloaders
 
 
-def get_train_test_datasets(idx: str, transform, target_transform=None) -> Tuple[Dataset, Dataset]:
+def get_train_test_datasets(idx: str, transform=None, target_transform=None,
+                            train_path=None, test_path=None, device=None, save_path=None) -> Tuple[Dataset, Dataset]:
     train_dataset, test_dataset = None, None
 
     if idx == 'mnist':
@@ -32,10 +40,82 @@ def get_train_test_datasets(idx: str, transform, target_transform=None) -> Tuple
         test_dataset = USPS('./data', train=False, transform=transform,
                             target_transform=target_transform, download=True)
     elif idx == 'cifar10':
-        train_dataset = CIFAR10('./data', train=True, transform=transform, 
+        train_dataset = CIFAR10('./data', train=True, transform=transform,
                                 target_transform=target_transform, download=True)
         test_dataset = CIFAR10('./data', train=False, transform=transform,
                                target_transform=target_transform, download=True)
+    elif idx == 'caltech256':
+        # Load the dataset using ImageFolder
+        dataset_path = './data/Caltech256'
+        dataset = ImageFolder(root=dataset_path, transform=transform, target_transform=target_transform)
+
+        # Split into train and test datasets (if needed)
+        train_size = int(0.9 * len(dataset))
+        test_size = len(dataset) - train_size
+        train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+    elif idx == 'stanforddogs':
+        dataset_path = './data/StanfordDogs'
+        dataset = ImageFolder(root=dataset_path, transform=transform, target_transform=target_transform)
+
+        # Split into train and test datasets (if needed)
+        train_size = int(0.9 * len(dataset))
+        test_size = len(dataset) - train_size
+        train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+    elif 'act' in idx:
+        dataset_idx = idx.split('-')[0]
+
+        if train_path is not None and test_path is not None:
+            etrain_data = torch.load(train_path, weights_only=False)
+            eval_data = torch.load(test_path, weights_only=False)
+            train_dataset = TensorDataset(etrain_data['data'], etrain_data['label'])
+            test_dataset = TensorDataset(eval_data['data'], eval_data['label'])
+            logging.info('train and test datasets are loaded from {} and {}'.format(train_path, test_path))
+        else:
+            if device is None:
+                device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            model = resnet18(weights=ResNet18_Weights.DEFAULT)
+            feature_extractor = torch.nn.Sequential(*list(model.children())[:-1]).to(device)
+            train_dataset, val_dataset = get_train_test_datasets(dataset_idx, transform)
+            train_loader, val_loader = get_dataloaders([train_dataset, val_dataset], batch_size=512,
+                                                       shuffle=False)
+            etrain_data, etrain_label, eval_data, eval_label = [], [], [], []
+            with torch.no_grad():
+                for data, label in tqdm(train_loader, desc='train data extraction', leave=True):
+                    data = data.to(device)
+                    edata = feature_extractor(data).to('cpu')
+                    etrain_data.append(edata.view(edata.shape[0], -1))
+                    etrain_label.append(label)
+                for data, label in tqdm(val_loader, desc='test data extraction', leave=True):
+                    data = data.to(device)
+                    edata = feature_extractor(data).to('cpu')
+                    eval_data.append(edata.view(edata.shape[0], -1))
+                    eval_label.append(label)
+            etrain_data = torch.cat(etrain_data, dim=0)
+            etrain_label = torch.cat(etrain_label, dim=0)
+            eval_data = torch.cat(eval_data, dim=0)
+            eval_label = torch.cat(eval_label, dim=0)
+            if save_path is not None:
+                tfname = '_'.join([dataset_idx, 'linear', 'resnet18', 'train.pth'])
+                tefname = '_'.join([dataset_idx, 'linear', 'resnet18', 'test.pth'])
+                tfname = os.path.join(save_path, tfname)
+                tefname = os.path.join(save_path, tefname)
+                torch.save(
+                    {
+                        'data': etrain_data,
+                        'label': etrain_label
+                    }, tfname
+                )
+
+                torch.save(
+                    {
+                        'data': eval_data,
+                        'label': eval_label
+                    }, tefname
+                )
+
+                logging.info('train and test datasets {}-act are saved under {}'.format(dataset_idx, save_path))
+            train_dataset = TensorDataset(etrain_data, etrain_label)
+            test_dataset = TensorDataset(eval_data, eval_label)
     try:
         assert (test_dataset is not None and train_dataset is not None), 'the given dataset id is not recognized'
     except AssertionError as error:
@@ -88,26 +168,40 @@ def __check_max_reached(sizes, max_sizes):
     return sizes, np.asarray(max_reached)
 
 
+# def _get_sizes(size, ratios, max_sizes):
+#     num_class = len(ratios)
+#     sizes = (ratios * size).astype(int)
+#
+#     sizes, max_reached = __check_max_reached(sizes, max_sizes)
+#     remainder = size - np.sum(sizes)
+#     for _ in range(remainder):
+#         if len(max_reached) > 0:
+#             available_classes = np.delete(np.arange(num_class), np.asarray(max_reached))
+#         else:
+#             available_classes = np.arange(num_class)
+#         sizes[np.random.choice(available_classes)] += 1
+#         sizes, max_reached = __check_max_reached(sizes, max_sizes)
+#         if len(max_reached) == num_class:
+#             break
+#
+#     assert np.sum(sizes) == size, 'size could not achieved'
+#     return sizes
+
 def _get_sizes(size, ratios, max_sizes):
-    num_class = len(ratios)
     sizes = (ratios * size).astype(int)
 
-    sizes, max_reached = __check_max_reached(sizes, max_sizes)
-    remainder = size - np.sum(sizes)
-    for _ in range(remainder):
-        sizes[np.random.choice(np.delete(np.arange(num_class), np.asarray(max_reached)))] += 1
-        sizes, max_reached = __check_max_reached(sizes, max_sizes)
-        if len(max_reached) == num_class:
-            break
-
-    assert np.sum(sizes) == size, 'size could not achieved'
+    decisive_idx = np.argmin(max_sizes - sizes)
+    dratio = ratios[decisive_idx]
+    dmax_size = max_sizes[decisive_idx]
+    dsize = dmax_size // dratio
+    sizes = (ratios * dsize).astype(int)
     return sizes
 
 
 def get_exact_surr_datasets(dataset,
                             target_size=None, target_ratios=None,
                             starget_size=None, starget_ratios=None,
-                            surr_dataset=None):
+                            surr_dataset=None, dirichlet=None, num_class=None):
     # TODO: errors might be explained later
     if surr_dataset is None and (target_ratios is not None and target_size is not None):
         num_class = len(target_ratios)
@@ -134,3 +228,28 @@ def get_exact_surr_datasets(dataset,
         return ConcatDataset(first_class_partitions), ConcatDataset(second_class_partitions)
     elif surr_dataset is not None:
         return dataset, surr_dataset
+    elif dirichlet is not None:
+        # generate class proportions for both exact and surrogate datasets
+        client_class_distributions = np.random.dirichlet(dirichlet * np.ones(num_class), size=2)
+        print(client_class_distributions)
+        return get_exact_surr_datasets(dataset, target_size=target_size, target_ratios=client_class_distributions[0],
+                                       starget_size=starget_size, starget_ratios=client_class_distributions[1])
+
+
+def get_transforms(idx):
+    if idx == 'cifar10':
+        return v2.Compose([
+            v2.Resize((224, 224), interpolation=InterpolationMode.BILINEAR),  # ResNet18 expects 224x224 input size
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalization for pretrained models
+        ])
+    elif idx == 'caltech256' or idx == 'stanforddogs':
+        return v2.Compose([
+            v2.Resize((224, 224), interpolation=InterpolationMode.BILINEAR),  # ResNet18 expects 224x224 input size
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),  # Normalization for pretrained models
+        ])
+    else:
+        return None
--- unknown_main.py
+++ unknown_main.py
@@ -7,15 +7,15 @@ import math
 
 import torch
 import torch.nn as nn
-import numpy as np
 
 from src.utils import set_seed
-from src.synthetic import GaussianDataset
-from src.data import get_retain_forget_datasets, get_dataloaders
+from src.data import (get_retain_forget_datasets, get_dataloaders, get_train_test_datasets,
+                      get_transforms, get_exact_surr_datasets)
 from src.loss import L2RegularizedCrossEntropyLoss
 from src.train import train
 from src.eval import evaluate
-from src.forget import forget
+from src.forget import forget, sample_from_exact_marginal, estimate_marginal_kl_distance
+
 
 def log_eval(model, train_loader, val_loader, retain_loader, forget_loader, surr_loader, criterion, device):
     train_acc = evaluate(train_loader, model, criterion, device=device, log=True)
@@ -23,7 +23,10 @@ def log_eval(model, train_loader, val_loader, retain_loader, forget_loader, surr
     retain_acc = evaluate(retain_loader, model, criterion, device=device, log=True)
     forget_acc = evaluate(forget_loader, model, criterion, device=device, log=True)
     surr_acc = evaluate(surr_loader, model, criterion, device=device, log=True)
-    logging.info('train: {}, test: {}, retain: {}, forget: {}, surrogate:{}'.format(train_acc, test_acc, retain_acc, forget_acc, surr_acc))
+    logging.info(
+        'train: {}, test: {}, retain: {}, forget: {}, surrogate:{}'.format(train_acc, test_acc, retain_acc, forget_acc,
+                                                                           surr_acc))
+
 
 def return_model(model_config, dim, num_class):
     if model_config['type'] == 'mlp':
@@ -42,27 +45,36 @@ def return_model(model_config, dim, num_class):
             model = nn.Linear(dim, num_class, bias=bias)
         return model
 
-def replace_none_with_None(d):
+
+def replace_none_with_none(d):
     for k, v in d.items():
         if isinstance(v, dict):
-            replace_none_with_None(v)
+            replace_none_with_none(v)
         elif v == 'none':
             d[k] = None
 
+
 def main():
-    parser = argparse.ArgumentParser(description='wrapper of all synthetic experiments')
+    parser = argparse.ArgumentParser(description='wrapper of all real dataset experiments')
     parser.add_argument('--config', type=str, required=True, help='path to config file')
     args = parser.parse_args()
 
     # read config set experiment
     with open(args.config, 'r') as file:
         config = yaml.safe_load(file)
-    replace_none_with_None(config)
+    replace_none_with_none(config)
     base_save_dir = config['setup']['base_save_dir']
+    about = config['setup']['about']
+    curr_dict = config
+    for key in about.split('-'):
+        curr_dict = curr_dict[key]
+    about_value = curr_dict
     if not os.path.exists(base_save_dir):
         os.makedirs(base_save_dir)
     now = datetime.now()
-    experiment_dir = os.path.join(base_save_dir, 'unkown-{}'.format(now.strftime('%Y-%m-%d-%H-%M-%S')))
+    experiment_dir = os.path.join(base_save_dir, '{}-{}-{}'.format(about,
+                                                                   str(about_value),
+                                                                   now.strftime('%Y-%m-%d-%H-%M-%S')))
     os.makedirs(experiment_dir)
     logging.basicConfig(filename=os.path.join(experiment_dir, 'experiment.log'), level=logging.INFO)
     logging.info('experiment started at %s', now.strftime('%Y-%m-%d %H:%M:%S'))
@@ -72,7 +84,7 @@ def main():
     with open(config_copy_path, 'w') as file:
         yaml.safe_dump(config, file)
     logging.info('configuration file copied to %s', config_copy_path)
-    
+
     # set seed
     seed = config['setup']['seed']
     set_seed(seed)
@@ -89,33 +101,41 @@ def main():
     data_config = config['data']
     exact_dataset_type = data_config['exact_dataset']
     surrogate_dataset_type = data_config['surrogate_dataset']
-    num_samples = data_config['num_samples']
     dim = data_config['dim']
     num_class = data_config['num_class']
-    if exact_dataset_type == 'gaussian':
-        exact_mean = np.zeros(dim)
-        exact_cov = np.eye(dim)
-        dataset = GaussianDataset(num_samples, num_class, exact_mean, exact_cov)
+    transforms = get_transforms(exact_dataset_type)
+    train_dataset, test_dataset = get_train_test_datasets(exact_dataset_type, transform=transforms,
+                                                         train_path=data_config['train_path'],
+                                                         test_path=data_config['test_path'],
+                                                         save_path=data_config['save_path'],
+                                                         device=device)
+    if surrogate_dataset_type is not None:
+        stransforms = get_transforms(surrogate_dataset_type)
+        surr_dataset, _ = get_train_test_datasets(surrogate_dataset_type, transform=transforms,
+                                                  train_path=data_config['strain_path'],
+                                                  test_path=data_config['stest_path'],
+                                                  save_path=data_config['ssave_path'],
+                                                  device=device)
+    else:
+        exact_size = int(len(train_dataset) / 2)
+        surr_size = len(train_dataset) - exact_size
+        dirichlet = data_config['dirichlet']
+        train_dataset, surr_dataset = get_exact_surr_datasets(train_dataset,
+                                                              target_size=exact_size,
+                                                              starget_size=surr_size,
+                                                              dirichlet=dirichlet, num_class=num_class)
 
-    if surrogate_dataset_type == 'gaussian' and exact_dataset_type == 'gaussian':
-        off_cov = data_config['off_cov']
-        surr_cov = exact_cov + off_cov * (np.ones_like(exact_cov) - np.eye(dim))
-        surr_dataset = dataset.create_surr(exact_mean, surr_cov)
-        kl_distance = surr_dataset.calculate_kl_between(dataset)
     logging.info('exact and surrogate dataset created')
-    logging.info('exact dataset size: {}, dim: {}'.format(num_samples, dim))
-    logging.info('surrogate dataset size: {}, dim: {}, off_cov: {}'.format(num_samples, dim, off_cov))   
-    logging.info('kl distance between surrogate and exact marginal distributions: {}'.format(kl_distance))
+    logging.info('exact dataset size: {}, dim: {}'.format(len(train_dataset), dim))
+    logging.info('surrogate dataset size: {}, dim: {}'.format(len(surr_dataset), dim))
     logging.info('#####################')
-    
+
     logging.info('#####################')
     logging.info('training setup')
     # train
     train_config = config['train']
 
     # set train test data
-    test_ratio = train_config['test_ratio']
-    train_dataset, test_dataset = get_retain_forget_datasets(dataset, test_ratio)
     forget_ratio = config['unlearn']['forget_ratio']
     retain_dataset, forget_dataset = get_retain_forget_datasets(train_dataset, forget_ratio)
     train_loader, test_loader = get_dataloaders([train_dataset, test_dataset], train_config['batch_size'])
@@ -123,8 +143,9 @@ def main():
     surr_loader = get_dataloaders(surr_dataset, train_config['batch_size'])
 
     logging.info('all dataloaders created')
-    logging.info('test ratio: {}, train dataset size: {}, test dataset size: {}'.format(test_ratio, len(train_dataset), len(test_dataset)))
-    logging.info('forget ratio: {}, retain dataset size: {}, forget dataset size: {}'.format(forget_ratio, len(retain_dataset), len(forget_dataset)))
+    logging.info(
+        'forget ratio: {}, retain dataset size: {}, forget dataset size: {}'.format(forget_ratio, len(retain_dataset),
+                                                                                    len(forget_dataset)))
 
     lambda_param = train_config['lambda']
     criterion = L2RegularizedCrossEntropyLoss(lambda_param)
@@ -140,18 +161,53 @@ def main():
     logging.info('lr: {}'.format(train_config['lr']))
     logging.info('#####################')
 
+    langevin_config = config['langevin']
+
     # train
     num_epochs = train_config['num_epochs']
     logging.info('#####################')
     logging.info('INITIAL TRAINING')
     train(train_loader, test_loader, model, criterion, optimizer, num_epoch=num_epochs, device=device)
     log_eval(model, train_loader, test_loader, retain_loader, forget_loader, surr_loader, criterion, device)
+    egensample_loader = sample_from_exact_marginal(model, langevin_config['num_samples'],
+                                                   langevin_config['input_size'],
+                                                   train_config['batch_size'],
+                                                   input_range=langevin_config['input_range'],
+                                                   max_iter=langevin_config['max_iter'],
+                                                   step_size=langevin_config['step_size'])
     model = model.to('cpu')
     # save model state dict
     model_save_path = os.path.join(experiment_dir, 'initial_model.pth')
     torch.save(model.state_dict(), model_save_path)
     logging.info('initial model state dict saved to %s', model_save_path)
     logging.info('#####################')
+
+    # surrogate training
+    logging.info('#####################')
+    logging.info('SURROGATE MODEL TRAINING')
+    smodel = return_model(model_config, dim, num_class)
+    smodel = smodel.to(device)
+    optimizer = torch.optim.Adam(smodel.parameters(), lr=train_config['lr'])
+    train(surr_loader, test_loader, smodel, criterion, optimizer, num_epoch=num_epochs, device=device)
+    log_eval(smodel, train_loader, test_loader, retain_loader, forget_loader, surr_loader, criterion, device)
+    sgensample_loader = sample_from_exact_marginal(smodel, langevin_config['num_samples'],
+                                                   langevin_config['input_size'],
+                                                   train_config['batch_size'],
+                                                   input_range=langevin_config['input_range'],
+                                                   max_iter=langevin_config['max_iter'],
+                                                   step_size=langevin_config['step_size'])
+    smodel = smodel.to('cpu')
+    model_save_path = os.path.join(experiment_dir, 'surrogate_model.pth')
+    torch.save(smodel.state_dict(), model_save_path)
+    logging.info('surrogate model state dict saved to %s', model_save_path)
+    logging.info('#####################')
+
+    # kl distance estimation
+    _, kl_distance = estimate_marginal_kl_distance(sgensample_loader, egensample_loader, device)
+    _.to('cpu')
+    del _
+
+    logging.info('kl distance estimated using generated samples is {}'.format(kl_distance))
 
     # retrain
     logging.info('#####################')
@@ -167,20 +223,6 @@ def main():
     logging.info('retrained model state dict saved to %s', model_save_path)
     logging.info('#####################')
 
-    # surrogate training
-    logging.info('#####################')
-    logging.info('SURROGATE MODEL TRAINING')
-    smodel = return_model(model_config, dim, num_class)
-    smodel = smodel.to(device)
-    optimizer = torch.optim.Adam(smodel.parameters(), lr=train_config['lr'])
-    train(surr_loader, test_loader, smodel, criterion, optimizer, num_epoch=num_epochs, device=device)
-    log_eval(smodel, train_loader, test_loader, retain_loader, forget_loader, surr_loader, criterion, device)
-    smodel = smodel.to('cpu')
-    model_save_path = os.path.join(experiment_dir, 'surrogate_model.pth')
-    torch.save(smodel.state_dict(), model_save_path)
-    logging.info('surrogate model state dict saved to %s', model_save_path)
-    logging.info('#####################')
-
     # unlearn config
     unlearn_config = config['unlearn']
     forget_ratio = unlearn_config['forget_ratio']
@@ -193,13 +235,16 @@ def main():
     hlip = unlearn_config['hlip']
     surr = unlearn_config['surr']
     known = unlearn_config['known']
-     
+    linear = unlearn_config['linear']
+
     # unlearn with exact
     logging.info('#####################')
     logging.info('UNLEARN WITH EXACT')
-    logging.info('noise --> eps_multiplier: {}, eps_power: {}, delta: {}, smooth: {}, sc: {}, lip: {}, hlip: {}'.format(forget_ratio, eps_multiplier, eps_power, delta, smooth, sc, lip, hlip))
+    logging.info('noise --> eps_multiplier: {}, eps_power: {}, delta: {}, smooth: {}, sc: {}, lip: {}, hlip: {}'.format(
+        forget_ratio, eps_multiplier, eps_power, delta, smooth, sc, lip, hlip))
     eps = eps_multiplier * (math.e ** eps_power)
-    umodel = forget(model, train_loader, forget_loader, forget_loader, criterion, device, save_path=experiment_dir, eps=eps, delta=delta, smooth=smooth, sc=sc, lip=lip, hlip=hlip)
+    umodel = forget(model, train_loader, forget_loader, forget_loader, criterion, device, save_path=experiment_dir,
+                    eps=eps, delta=delta, smooth=smooth, sc=sc, lip=lip, hlip=hlip, linear=linear)
     log_eval(umodel, train_loader, test_loader, retain_loader, forget_loader, surr_loader, criterion, device)
     umodel = umodel.to('cpu')
     model_save_path = os.path.join(experiment_dir, 'uexact_model.pth')
@@ -211,10 +256,14 @@ def main():
         # unlearn with surrogate
         logging.info('#####################')
         logging.info('UNLEARN WITH SURROGATE')
-        logging.info('noise --> eps_multiplier: {}, eps_power: {}, delta: {}, smooth: {}, sc: {}, lip: {}, hlip: {}, kl_distance: {}'.format(forget_ratio, eps_multiplier, eps_power, delta, smooth, sc, lip, hlip, kl_distance))
+        logging.info(
+            'noise --> eps_multiplier: {}, eps_power: {}, delta: {}, smooth: {}, sc: {}, lip: {}, hlip: {}, kl_distance: {}'.format(
+                forget_ratio, eps_multiplier, eps_power, delta, smooth, sc, lip, hlip, kl_distance))
         smodel = smodel.to(device)
-        usmodel = forget(model, surr_loader, forget_loader, forget_loader, criterion, device, save_path=experiment_dir, eps=eps, delta=delta, smooth=smooth, sc=sc, lip=lip, hlip=hlip, surr=surr,
-                         known=known, surr_loader=surr_loader, surr_model=smodel, kl_distance=kl_distance)
+        usmodel = forget(model, surr_loader, forget_loader, forget_loader, criterion, device, save_path=experiment_dir,
+                         eps=eps, delta=delta, smooth=smooth, sc=sc, lip=lip, hlip=hlip, surr=surr,
+                         known=known, surr_loader=surr_loader, surr_model=smodel, kl_distance=kl_distance,
+                         linear=linear)
         log_eval(usmodel, train_loader, test_loader, retain_loader, forget_loader, surr_loader, criterion, device)
         usmodel = usmodel.to('cpu')
         smodel = smodel.to('cpu')
