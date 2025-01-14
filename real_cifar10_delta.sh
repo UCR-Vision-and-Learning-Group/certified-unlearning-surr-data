fb6770005117cd1c256852ae6f226ff0dbc0b425
umityigitbsrn <umityigitbsrn@gmail.com>
2025-01-14 12:57:16 -0800

    cifar10 exp scripts

---
+++ real_cifar10_delta.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Directory containing the configuration files
+CONFIG_DIR="./certified-unlearning-surr-data/configs/real/cifar10/delta"
+
+# Python script to run with each configuration file
+PYTHON_SCRIPT="./certified-unlearning-surr-data/real_main.py"
+
+# device: 0
+
+# Loop through each configuration file in the directory
+for config_file in "$CONFIG_DIR"/*.yml; do
+  echo "Running configuration: $config_file"
+  python "$PYTHON_SCRIPT" --config "$config_file"
+done
