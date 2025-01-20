217d5a1b69011fbffb03ce3e95cbbb233cdaa5f6
umityigitbsrn <umityigitbsrn@gmail.com>
2025-01-20 04:12:06 -0800

    real configs are added

---
+++ cuda0.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# device: 0
+
+DATASET="sdogs"
+EXP="delta"
+MODE="real"
+CONFIG_DIR="./configs/"$MODE"/"$DATASET"/"$EXP""
+PYTHON_SCRIPT="./real_main.py"
+
+for config_file in "$CONFIG_DIR"/*.yml; do
+  if [ -e "$config_file" ]; then
+    echo "Running configuration: $config_file"
+    python "$PYTHON_SCRIPT" --config "$config_file"
+  else
+    echo "No configuration files found in $CONFIG_DIR"
+    break
+  fi
+done
