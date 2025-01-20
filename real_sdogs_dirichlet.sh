f9276742f09fb978f8122e3cdf73d3387e88ceee
umityigitbsrn <umityigitbsrn@gmail.com>
2025-01-19 17:45:03 -0800

    dir experiments for cifar100 and sdogs

--- real_sdogs_dirichlet.sh
+++ real_sdogs_dirichlet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Directory containing the configuration files
-CONFIG_DIR="/home/umityigitbsrn/Desktop/umityigitbsrn/certified-unlearning-surr-data/configs/real/cifar10/delta"
+CONFIG_DIR="/home/umityigitbsrn/Desktop/umityigitbsrn/certified-unlearning-surr-data/configs/real/sdogs/dirichlet"
 
 # Python script to run with each configuration file
 PYTHON_SCRIPT="/home/umityigitbsrn/Desktop/umityigitbsrn/certified-unlearning-surr-data/real_main.py"
 
-# device: 0
+# device: 3
 
 # Loop through each configuration file in the directory
 for config_file in "$CONFIG_DIR"/*.yml; do
